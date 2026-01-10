e92924e1dbb132a254f31c0e48ec0f2791a6a055
James Richardson <james@time4tea.net>
2026-01-10 09:59:19 +0000

    try to make python a bit better (first of many)

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import time
 from machine import Pin
 
+from display import EPD_2in9
+from writer import  Writer
+
 led = Pin("LED", Pin.OUT)
 
-while 1:
-    led.low()
-    time.sleep(0.25)
-    led.high()
-    time.sleep(0.25)
+epd = EPD_2in9(greyscale=True)
 
+# epd.clear(0xff)
+# epd.TurnOnDisplay()
+
+epd.init()
+epd.fb.fill_rect(0, 0, 127, 74, epd.black)
+epd.fb.text('GRAY1', 10, 33, epd.white)
+epd.fb.fill_rect(0, 74, 127, 74, epd.darkgray)
+epd.fb.text('GRAY2', 10, 107, epd.grayish)
+epd.fb.fill_rect(0, 148, 127, 74, epd.grayish)
+epd.fb.text('GRAY3', 10, 181, epd.darkgray)
+epd.fb.fill_rect(0, 222, 127, 74, epd.white)
+epd.fb.text('GRAY4', 10, 255, epd.black)
+epd.display()
 
