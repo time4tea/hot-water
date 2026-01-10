ee542974e5623e781c59e30fb921ee6f37215550
James Richardson <james@time4tea.net>
2026-01-10 15:56:16 +0000

    add direction arrow

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import machine
 from machine import Pin
-from network import WLAN
 
+import images
+import ubuntu
 import umqtt.simple
 from display import EPD_2in9
+from config import config
 from writer import Writer
-from secrets import secrets
-import ubuntu
+
 
 def do_connect(ssid: str, key: str):
     import machine, network
@@ -25,16 +26,23 @@ class Display:
         self.epd = epd
         self.w = w
 
+    def clear(self, colour=None):
+        self.epd.fb.fill_rect(0, 0, 296, 128, colour if colour is not None else self.epd.white)
+
     def show_status(self, text):
-        self.epd.fb.fill_rect(0, 0, 296, 128, self.epd.black)
+        self.clear(self.epd.black)
         self.epd.fb.text(text, 0, 0, self.epd.white)
-        self.epd.display()
 
     def show_temp(self, value):
-        self.epd.fb.fill_rect(0, 0, 296, 128, self.epd.white)
-        self.w.set_textpos(20,10)
+        self.w.set_textpos(20, 10)
         self.w.printstring(value, invert=True)
+
+    def image(self, image: images.Bitmap):
+        image.blit(self.epd.fb, 296 - (image.width + 4), (128 // 2) - (image.height // 2), self.epd.black, self.epd.white)
+
+    def update(self):
         self.epd.display()
+
 
 led = Pin("LED", Pin.OUT)
 
@@ -46,18 +54,37 @@ d = Display(epd, wu)
 epd.init()
 
 d.show_status("Connecting...")
+d.update()
 
-do_connect(secrets["ssid"], secrets["pw"])
+do_connect(config["wlan"]["ssid"], config["wlan"]["pw"])
+
+last_temp = None
+
 
 def cb(topic, value):
+    global last_temp
     print(value)
-    temp = f"{float(value):#.1f}"
-    d.show_temp(temp)
 
-mq = umqtt.simple.MQTTClient(client_id="therm", server="192.168.0.95")
+    temp = float(value)
+    d.clear()
+    d.show_temp(f"{temp:#.1f}")
+
+    if last_temp is not None:
+        if temp < last_temp:
+            d.image(images.down)
+        else:
+            d.image(images.up)
+
+    last_temp = temp
+    d.update()
+
+
+mq = umqtt.simple.MQTTClient(client_id=config["mqtt"]["name"], server=config["mqtt"]["server"])
 mq.connect()
 
+d.clear()
 d.show_temp("??")
+d.update()
 
 mq.set_callback(cb)
 mq.subscribe("sensor.hw.temp")
@@ -66,5 +93,3 @@ while True:
     r = mq.wait_msg()
     if r is None:
         machine.soft_reset()
-
-
